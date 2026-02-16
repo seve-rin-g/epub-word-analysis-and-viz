cdfb21bb14b15190be41e75c377dee28fd2d2337
rin <rng@rins-MacBook-Pro.local>
2026-02-16 12:23:52 -0800

    logging

--- postgres.py
+++ postgres.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, text
 from faker import Faker
-import time
+import time, logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 def get_db_engine():
     # create engine for connecting to PostgreSQL
@@ -13,17 +16,16 @@ while True:
             print("Database connection successful!")
             break
     except Exception as e:
-        print(f"Database connection failed {str(e)}.")
+        LOGGER.warning(f"Database connection failed {str(e)}.")
         time.sleep(5)  # wait for 5 seconds before retrying
 
 faker = Faker('en_US')
 
 for i in range(10):
-    print(f"inserting record {i}")
     # insert fake data into the database
     query = f"INSERT INTO ol_bookdb.book (title, author) VALUES ('{faker.sentence()}', '{faker.name()}')"
     db_engine.execute(text(query))
-    print (f"record {i} inserted: {query}")
+    LOGGER.info(f"record {i} inserted: {query}")
 
 db_engine.commit() # commit the transaction
 db_engine.close()
