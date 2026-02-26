3c22d82c470b3776ca5e3e614295c69f4e58196d
rin <rng@rins-MBP.localdomain>
2026-02-26 12:10:13 -0800

    words are lemmatized before adding to freqdict

--- services/web/project/wordprocessing.py
+++ services/web/project/wordprocessing.py
@@ -1,7 +1,21 @@
 
+import re
 import ebooklib
 from ebooklib import epub
-import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+# Download required NLTK data if not already present
+try:
+    nltk.data.find('corpora/wordnet')
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('wordnet')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    
+from nltk.corpus import wordnet as wn
 
 def read_epub_file(filepath):
     book = epub.read_epub(filepath)
@@ -15,26 +29,36 @@ def read_epub_file(filepath):
 def word_frequency_analysis(alltext):
     # Simple word frequency analysis using regex to split words
     alltext = re.sub(r'<[^>]+>', '', alltext)     # remove html tags
-    words = re.findall(r'\b\w+\b', alltext.lower())
+    words = re.findall(r'\b\w+\b', alltext.lower()) # regex version
+    pos_tagged = nltk.pos_tag(words)
     frequency = {}
-    for word in words:
-        frequency[word] = frequency.get(word, 0) + 1
+    for word, pos in pos_tagged:
+        if word.isdigit():
+            continue    
+        stemmedword = WordNetLemmatizer().lemmatize(word, get_wordnet_pos(pos[0].upper())) # stem the word to its root form
+        frequency[stemmedword] = frequency.get(stemmedword, 0) + 1
     # Sort by word
     sorted_freq = sorted(frequency.items(), key=lambda item: item[0])
-    # classify each word as an article, noun, adjective, verb, or pronoun (very basic classification based on simple heuristics)
-    classified_freq = []
-    for word, freq in sorted_freq:
-        if word in ['he', 'she', 'it', 'they', 'we', 'i', 'you']:
-            classified_freq.append((word, freq, 'pronoun'))
-        elif word.endswith('ing') or word.endswith('ed'):
-            classified_freq.append((word, freq, 'verb'))
-        elif word.endswith('ly') or word.endswith('ous') or word.endswith('ful'):
-            classified_freq.append((word, freq, 'adjective'))
-        elif word not in ['the', 'a', 'an']:  
-            classified_freq.append((word, freq, 'noun'))
-    return classified_freq
+    
+    return sorted_freq
 
 def semantic_grouping(word_freq):
     # Placeholder for semantic grouping logic
     # use NLTK or spaCy to determine word meanings and group them
     return word_freq
+
+def get_wordnet_pos(treebank_tag):
+        """
+        return WORDNET POS compliance to WORDENT lemmatization (a,n,r,v) 
+        """
+        if treebank_tag.startswith('J'):
+            return wn.ADJ
+        elif treebank_tag.startswith('V'):
+            return wn.VERB
+        elif treebank_tag.startswith('N'):
+            return wn.NOUN
+        elif treebank_tag.startswith('R'):
+            return wn.ADV
+        else:
+            # As default pos in lemmatization is Noun
+            return wn.NOUN
